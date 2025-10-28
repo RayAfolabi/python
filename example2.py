59b252d6253b55c929c1212fd57e7a008060286a
root <root@localhost>
2025-10-28 13:35:55 +0100

    auto publish 1761654955799

--- example2.py
+++ example2.py
@@ -5,3 +5,9 @@ float_text=input('input a float number:')
 float_num=float(flaot_text)
 result=int_num*float_num
 print('print result:', result)
+
+#A better way of writing the code
+int_num=int(input('enter an interger number:' ))
+float_num=float(input('enter a float number:'))
+result=int_num*float_num
+print('your result is:', result)
