8f1c5d3070991a27f56da6afff936b04665ea750
root <root@localhost>
2025-10-28 13:16:07 +0100

    auto publish 1761653766764

---
+++ example2.py
@@ -0,0 +1,7 @@
+#word problem; Take two input from a user, one will be an integer and the other a float, then multiple the result to get an output 
+int_text=input('input an interger number:')
+int_num=int(int_text)
+float_text=input('input a float number:')
+float_num=float(flaot_text)
+result=int_num*float_num
+print('print result:', result)
---
+++ src/example.py
@@ -0,0 +1,87 @@
+
+print("Rachael Afolabi")
+
+17%3
+price = 17
+if price < 10:
+    print("I want the food")
+print("I want the drinks")
+
+Jennifer_money=400
+julia_money=1200
+
+if Jennifer_money>julia_money:
+    print('aweasom')
+else:
+    print('not good')
+first_nums = [1,2,3,4]
+second_nums = [5,6,7,8]
+all_nums = first_nums + second_nums
+print(all_nums)
+
+nums = [121, 63, 1, -15, 36, 11]
+nums.sort()
+print(nums)
+num=[121,63,4,-2,36,11]
+num.sort()
+print(num)
+basket = ["Apple", "Orange", "Strawberry", "Banana"]
+
+for fruit in basket:
+   print(fruit)
+#continue will skip the negative numbers 
+nums=[23,64,81,-3,-4,5]
+for num in nums
+if num<0:
+    continue 
+    print(num)
+    
+
+nums = [1, 22, 35, -21, 45, 16, 18]
+for num in nums
+    if num %2=0:
+      continue
+   print(num)
+
+
+count=0
+while count<5:
+  print(count)
+  count=count+1
+  i = 1
+sum = 0
+while i <= 50:
+    if i%2 != 0:
+        sum = sum + i
+    i = i+1
+
+print(sum)
+
+i=1
+sum=0
+while i<=0:
+     if i%2 !=0:
+         sum =sum +1
+     i=i+1
+print(sum)
+#the use of break while loop
+count = 0
+while count <15:
+   if count == 5:
+      break
+   print(count)
+   count += 1
+   
+  
+  count = 0
+while count <= 10:
+   count = count + 1
+   if count % 2 == 0:
+      continue
+   print(count)
+   
+ def add_numbers(first, second):
+   sum = first + second
+   print(sum)
+
+add_numbers(13, 53) 
