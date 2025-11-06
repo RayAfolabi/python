0417bb52ad4ce11009c1f484dcd3c1bbe0aedf41
root <root@localhost>
2025-11-06 12:00:31 +0100

    auto publish 1762426831640

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
-class Shopping:
-    cart = [ ]
-    def add_to_cart(self, item):
-        self.cart.append(item)
-        
-customer1 = Shopping()
-customer1.add_to_cart('T-shirt')
-print(customer1.cart)
+class Bank:
+    def __init__(self):
+        self.minimum = 100
+        self.balance = 1000
+    def get_balance(self):
+        return self.balance
+    def withdraw(self, amount):
+        if amount < self.minimum:
+            print('No money for you')
+        else:
+            self.balance = self.balance - amount
+            return amount
 
-        
-      
+
+my_bank = Bank()
+my_bank.withdraw(100)
+balance = my_bank.get_balance()
+print(balance)
+my_bank.withdraw(50)
+balance = my_bank.get_balance()
+print(balance)
